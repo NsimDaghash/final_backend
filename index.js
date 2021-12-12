1581b75f43ad571f5c4782855dc07fc411473106
nasimdaghash <87161661+nasimdaghash@users.noreply.github.com>
2021-12-12 15:47:18 +0200

    first commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
 // const CONNECTION_URL = 'mongodb+srv://nasimd:2596nwsh@cluster0.ti98j.mongodb.net/social?retryWrites=true&w=majority';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000 ;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   //.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
